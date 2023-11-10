72efc4e39ae156414e61cac40dc0b54f5a8a0d32
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2023-11-11 00:46:16 +0500

    test lint staged 4

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ function login(username: string): string {
     const user = {
         name: 'mohsin',
     };
-    const name = user['name'];
+
+    const name = user.name;
+
     return username + name;
 }
 
