96e5dfbd0ae5f24366d1f11443ccce048fffe598
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2023-11-13 17:23:21 +0500

    logger setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 const startServer = () => {
     const port = Config.PORT;
     try {
-        app.listen(port, () => {
-            console.log(`Listening on port ${port}`);
-        });
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
+        app.listen(port, () => logger.info(`Listening on port ${port}`));
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 3000);
+        }
     }
 };
 startServer();
